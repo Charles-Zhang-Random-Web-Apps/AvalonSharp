4995397da79a39947ebc8de0d89967c44e9bd64c
Charles Zhang <charles.zhang@blackbirdinteractive.com>
2022-02-18 00:16:55 -0800

    Basic setup

---
+++ Models/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AvalonSharp.Models
+{
+    public class Team
+    {
+        public Member Leader { get; set; }
+        public HashSet<Member> Teammates { get; set; } = new HashSet<Member>();
+        public Dictionary<Member, Vote> Votes = new Dictionary<Member, Vote>();
+        public bool? Success { get; set; } = null;
+    }
+}
---
+++ Models/Vote.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AvalonSharp.Models
+{
+    public class Vote
+    {
+        public Member Voter { get; set; }
+        public bool InFavor { get; set; }
+
+        public Vote(Member voter, bool inFavor)
+        {
+            Voter = voter;
+            InFavor = inFavor;
+        }
+    }
+}
