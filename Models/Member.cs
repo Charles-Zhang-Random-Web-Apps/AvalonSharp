95e18fabc74b0f4f1d51f6258da12405929c6431
Charles Zhang <charles.zhang@blackbirdinteractive.com>
2022-02-18 14:34:23 -0800

    Update style; Optimize display; Provide analysis

--- Models/Member.cs
+++ Models/Member.cs
@@ -9,7 +9,13 @@ namespace AvalonSharp.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+
+        #region Game Session Data
         public bool IsGroupA { get; set; }
         public string MemberNote { get; set; }
+        public bool IsLikelySpy { get; set; }
+        public bool IsLikelyAccomplice { get; set; }
+        public bool IsLikelySpyLead { get; set; }
+        #endregion
     }
 }
