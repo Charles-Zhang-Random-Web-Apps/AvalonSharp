b1687b515658fe262b4cb8522bf8ff75a86fe9ae
Charles Zhang <charles.zhang@blackbirdinteractive.com>
2022-02-18 10:00:27 -0800

    Add listening to form factor change

---
+++ wwwroot/js/app.js
@@ -0,0 +1,18 @@
+﻿function isDevice() {
+    return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent);
+}
+
+function isSmallFormFactor() {
+    return document.body.clientWidth < 960;
+}
+
+var dotNetHelper = null;
+function registerDotNetHelper(helper) {
+    dotNetHelper = helper;
+}
+function pollScreenChange(event) {
+    if (dotNetHelper != null) {
+        dotNetHelper.invokeMethodAsync('performScreenChange', isSmallFormFactor());
+    }
+}
+window.addEventListener("resize", pollScreenChange);
